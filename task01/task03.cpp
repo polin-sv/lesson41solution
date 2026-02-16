e4062035834c55b9ad6bb09c8f0616271e1d209c
polin-sv <polinasvetlay@icloud.com>
2026-02-16 18:38:23 +0300

    task01 update

--- task01/task03.cpp
+++ task01/task03.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define STUDENTS_NUMBER 3.0
+#define DEFAULT_SIZE 20
 
 using namespace std;
 
@@ -11,22 +11,28 @@ double calculate_marks(double a, double b, double c) {
 
 int main() {
 
-	const int MARK_COUNT = 3;
-	int marks[MARK_COUNT];
+	int marks[DEFAULT_SIZE];
+
+	int length;
+
+	do {
+		cout << "input the number of marks(1-20): ";
+		cin >> length;
+	} while (length <= 0 && length > DEFAULT_SIZE);
 
 	cout << "input three marks: ";
-	for (int index = 0; index < MARK_COUNT; index++) {
+	for (int index = 0; index <length; index++) {
 		cin >> marks[index];
 	}
 
 	double avg = 0;
 
-	for (int index = 0; index < MARK_COUNT; index++) {
+	for (int index = 0; index < length; index++) {
 
 		avg += marks[index];
 	}
 
-	cout << "avg result is  " << avg / MARK_COUNT << endl;
+	cout << "avg result is  " << avg / length << endl;
 
 	return 0;
 }
