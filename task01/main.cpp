d44a15d12b741e72833a864936d990d6eea9d795
polin-sv <polinasvetlay@icloud.com>
2026-02-16 17:04:16 +0300

    update

--- task01/main.cpp
+++ task01/main.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
-using namespace std;
-
-double calculate_marks(double a, double b, double c) {
-
-	return (a + b + c) / 3.0;
-}
-
-int main() {
-
-	double mark1, mark2, mark3;
-
-	cout << "input three marks: ";
-	cin >> mark1 >> mark2 >> mark3;
-
-	double result= calculate_marks(mark1, mark2, mark3);
-
-	cout << "result: " << result;
-
-	return 0;
-}
+//#include <iostream>
+//#define STUDENTS_NUMBER 3.0
+//
+//using namespace std;
+//
+//
+//double calculate_marks(double a, double b, double c) {
+//
+//	return (a + b + c) / STUDENTS_NUMBER;
+//}
+//
+//int main() {
+//
+//	double mark1, mark2, mark3;
+//
+//	cout << "input three marks: ";
+//	cin >> mark1 >> mark2 >> mark3;
+//
+//	double result= calculate_marks(mark1, mark2, mark3);
+//
+//	cout << "result: " << result;
+//
+//	return 0;
+//}
